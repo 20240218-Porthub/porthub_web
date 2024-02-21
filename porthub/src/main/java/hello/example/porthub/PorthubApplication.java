5beee5f38eb2d3838e7d8ef2ec1cda7e02d9dbef
JeongHyeon Jo <cisxo@naver.com>
2024-02-22 00:15:35 +0900

    first_commit

---
+++ porthub/src/main/java/hello/example/porthub/PorthubApplication.java
@@ -0,0 +1,12 @@
+package hello.example.porthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorthubApplication.class, args);
+	}
+}
---
+++ porthub/src/test/java/hello/example/porthub/PorthubApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.example.porthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
