cf2c26b0b9309aeb788efb2b993ee009069aabe7
JeongHyeon Jo <cisxo@naver.com>
2024-03-12 14:51:04 +0900

    test

---
+++ porthub/src/main/java/hello/example/porthub/controller/IndexController.java
@@ -0,0 +1,19 @@
+package hello.example.porthub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(value = {"/index2"})
+    public String index2() {
+        return "index2";
+    }
+}
+
