c09d09e5d557ee28b292f4c4628deb90cfee0077
JeongHyeon Jo <cisxo@naver.com>
2024-03-15 03:12:31 +0900

    feat: added View Resolver

--- porthub/src/main/java/hello/example/porthub/controller/IndexController.java
+++ porthub/src/main/java/hello/example/porthub/controller/IndexController.java
@@ -1,19 +1,26 @@
 package hello.example.porthub.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping
+@RequiredArgsConstructor
 public class IndexController {
 
-    @GetMapping(value = {"/", "/index"})
+    @GetMapping(value = {"/", "/main"})
     public String index() {
-        return "index";
+        return "portfolio/main";
     }
 
-    @GetMapping(value = {"/index2"})
-    public String index2() {
-        return "index2";
+
+    @GetMapping(value = {"/login"})
+    public String login() {
+        return "register/login";
     }
+
 }
 
