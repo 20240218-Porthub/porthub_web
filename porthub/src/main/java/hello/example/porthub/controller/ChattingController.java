ad0eaa68254067897634960f0b389144a9069bea
이연재 <leeyj0304@gmail.com>
2024-04-03 17:33:43 +0900

    feat: Add gradle dependencies for websocket

--- porthub/src/main/java/hello/example/porthub/controller/ChattingController.java
+++ porthub/src/main/java/hello/example/porthub/controller/ChattingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChattingController {
-
+    
     @GetMapping("/user/chat")
     public String chatGet(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         // userDetails 객체에서 현재 로그인한 사용자의 이름(Username)을 가져옴
