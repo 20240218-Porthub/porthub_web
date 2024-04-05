0994c863c2851d5b4d1041e91650c297cf7e49b3
JeongHyeon Jo <cisxo@naver.com>
2024-04-06 06:45:52 +0900

    feat: annotation

--- porthub/src/main/java/hello/example/porthub/controller/PortfolioController.java
+++ porthub/src/main/java/hello/example/porthub/controller/PortfolioController.java
@@ -21,9 +21,14 @@ public class PortfolioController {
     public String uploadPortfolio(@ModelAttribute PortfolioDto portfolioDto) throws IOException {
         System.out.println("portfolioDto= " + portfolioDto);
         System.out.println("portfolioimage,content" + portfolioDto.getContent() + portfolioDto.getFile());
+        // form data 전송시 List 형태로 담아두었음. getContent(), getFile()시 여러개의 파일이 담겨있음
+        // 이를 순서대로 db에 저장하는 로직을 구현해야 함.
+
+
         String thumbnailUrl = s3Service.uploadThumbnail(portfolioDto.getThumbnail_url());
         System.out.println(thumbnailUrl);
         // 이 경로를 service단에 전달해서 db에 저장해야함. repository상에서 Multiple 타입으로 작성되어있음
+
         int uploadResult = portfolioService.upload(portfolioDto);
         if (uploadResult > 0) {
             return "redirect:/portfolio/main";
