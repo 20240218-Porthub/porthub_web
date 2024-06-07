87e64a1abed8fe80f11f734c09993d7078608ea6
hongseob <ghdtjq1111@gmail.com>
2024-06-07 14:32:50 +0900

    feat:mentoring import princiipal

--- porthub/src/main/java/hello/example/porthub/controller/IndexController.java
+++ porthub/src/main/java/hello/example/porthub/controller/IndexController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.security.Principal;
 import java.util.*;
 
 
