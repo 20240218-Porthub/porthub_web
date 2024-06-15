61efd3de91d9ea431c961f40f35674c5eced6418
JeongHyeon Jo <cisxo@naver.com>
2024-06-15 16:45:01 +0900

    err: null_follow_list

--- porthub/src/main/java/hello/example/porthub/controller/IndexController.java
+++ porthub/src/main/java/hello/example/porthub/controller/IndexController.java
@@ -386,18 +386,21 @@ public class IndexController {
 
 
     @PostMapping("/follow/{followingID}")
-    public String portsFollowInsert(@PathVariable("followingID") int followingID) {
+    public String portsFollowInsert(@PathVariable("followingID") int followingID, HttpServletRequest request) {
         String CurrentUseremail = SessionUtils.getCurrentUsername();
         portfolioService.following(followingID, CurrentUseremail);
 
-        return "redirect:/main";
+        String referer = request.getHeader("Referer"); // 이전 페이지의 URL을 가져옴
+        return "redirect:" + referer; // 이전 페이지로 리디렉션
     }
 
     @PostMapping("/unfollow/{followingID}")
-    public String portsFollowDelete(@PathVariable("followingID") int followingID) {
+    public String portsFollowDelete(@PathVariable("followingID") int followingID, HttpServletRequest request) {
         String CurrentUseremail = SessionUtils.getCurrentUsername();
         portfolioService.unfollow(followingID, CurrentUseremail);
-        return "redirect:/main";
+
+        String referer = request.getHeader("Referer"); // 이전 페이지의 URL을 가져옴
+        return "redirect:" + referer; // 이전 페이지로 리디렉션
     }
 
 }
