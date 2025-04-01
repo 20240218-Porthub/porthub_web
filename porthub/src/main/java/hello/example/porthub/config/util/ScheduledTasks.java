6fbaadc7a263c4a2a8d7a2900f6875c1d8b1927f
JeongHyeon Jo <cisxo@naver.com>
2025-04-01 23:51:36 +0900

    refactor: Scheduled Async && Cors

--- porthub/src/main/java/hello/example/porthub/config/util/ScheduledTasks.java
+++ porthub/src/main/java/hello/example/porthub/config/util/ScheduledTasks.java
@@ -2,6 +2,7 @@ package hello.example.porthub.config.util;
 
 import hello.example.porthub.service.PortfolioService;
 import jakarta.annotation.PostConstruct;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,8 @@ public class ScheduledTasks {
     }
 
     @Scheduled(cron = "0 0 0 * * *")
-    @PostConstruct
+//    @PostConstruct
+    @Async
     public void PopularTask() {
 //      정각 12시마다 업데이트
         portfolioService.updatePopularTask();
